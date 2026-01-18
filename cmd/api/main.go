d2d59536510171cddba271196af6aa8e83857237
sulaimondawood <sulaimondawod@gmail.com>
2026-01-18 15:35:41 +0100

    first commit

---
+++ cmd/api/main.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+
+	router := gin.Default()
+
+	router.GET("/", func(ctx *gin.Context) {
+
+		ctx.JSON(200, gin.H{
+			"message": "Todo api is running",
+			"status":  "success",
+		})
+
+	})
+
+	router.Run(":8080")
+
+}
